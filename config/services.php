663280609f20e04ebc57dccbb4484a99e9962bdf
Tolulope Akinnuoye <tolulope.akinnuoye@mysourcebank.com>
2024-11-16 05:50:42 +0100

    Implement scheduled commands to regularly fetch and update

--- config/services.php
+++ config/services.php
@@ -35,4 +35,14 @@ return [
         ],
     ],
 
+    'newsapi' => [
+        'key' => env('NEWS_API_KEY'),
+    ],
+    'guardian' => [
+        'key' => env('GUARDIAN_API_KEY'),
+    ],
+    'nyt' => [
+        'key' => env('NYT_API_KEY'),
+    ],
+
 ];
